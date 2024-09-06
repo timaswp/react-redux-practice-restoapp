90049db387e2f869409e77f8093ade6e424926a1
Teymur <melikovteymur@mail.ru>
2024-09-06 19:08:09 +0400

    react router and server interaction

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc';
+import { Routes, Route } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = ({RestoService}) => {
-    console.log(RestoService.getMenuItems());
+const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Routes>
+                <Route path='/' Component={MainPage}/>
+                <Route path='/cart' Component={CartPage}/>
+            </Routes>
         </div>
     )
 }
 
-export default WithRestoService()(App);
+export default (App);
